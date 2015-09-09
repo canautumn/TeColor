819c8897415974e51591f20caff0db851b4b3613
Can <kfjincan@gmail.com>
2015-09-09 17:35:41 -0400

    Change build target to static library and a demo executable.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include "ColorMatchingFunction.h"
-#include "Illuminant.h"
-#include "ReflectanceSet.h"
 #include "SpectralKit.h"
 
 using namespace std;
 using namespace tecolor;
 
 int main() {
-  cout << "Hello, World!" << endl;
   auto cmf19312d = ColorMatchingFunction(Observer1931At2Deg);
   cout << "Color Matching Function of Observer 1931 at 2 Degree:" << endl;
   cout << cmf19312d.cmf() << endl;
